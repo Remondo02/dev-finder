1b107313301d246f2dff9e5c4661a5eba67f106c
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2024-05-06 16:37:01 +0900

    Extend session type

--- src/app/create-room/actions.ts
+++ src/app/create-room/actions.ts
@@ -4,8 +4,12 @@ import { db } from '@/db'
 import { Room, room } from '@/db/schema'
 import { getSession } from '@/lib/auth'
 
-export async function createRoomAction(roomData: Omit<Room, 'userId'>) {
+export async function createRoomAction(roomData: Omit<Room, 'id' | 'userId'>) {
   const session = await getSession()
-  console.log(session)
-  await db.insert(room).values({ ...roomData, userId: 'TODO' })
+
+  if (!session) {
+    throw new Error('you must be logged in to create this room')
+  }
+
+  await db.insert(room).values({ ...roomData, userId: session.user.id })
 }
