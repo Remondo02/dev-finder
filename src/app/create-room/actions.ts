730663caaa72ed841f198ba490eb6382191c113d
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2024-05-06 16:13:09 +0900

    Creating the room form

---
+++ src/app/create-room/actions.ts
@@ -0,0 +1,11 @@
+'use server'
+
+import { db } from '@/db'
+import { Room, room } from '@/db/schema'
+import { getSession } from '@/lib/auth'
+
+export async function createRoomAction(roomData: Omit<Room, 'userId'>) {
+  const session = await getSession()
+  console.log(session)
+  await db.insert(room).values({ ...roomData, userId: 'TODO' })
+}
