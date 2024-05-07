6f7bb42307b12b6764915075731b200430b2c1d8
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2024-05-07 15:26:32 +0900

    Header

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -3,20 +3,66 @@
 import { ModeToggle } from '@/components/mode-toggle'
 import { Button } from '@/components/ui/button'
 import { signIn, signOut, useSession } from 'next-auth/react'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { LogInIcon, LogOutIcon } from 'lucide-react'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import Image from 'next/image.js'
+import Link from 'next/link.js'
+
+function AccountDropdown() {
+  const session = useSession()
+  const isLoggedIn = !!session.data
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant='link'>
+          <Avatar className='mr-2'>
+            <AvatarImage src={session.data?.user.image ?? ''} />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          {session.data?.user.name}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        {isLoggedIn ? (
+          <DropdownMenuItem onClick={() => signOut()}>
+            <LogOutIcon className='mr-2' /> Sign Out
+          </DropdownMenuItem>
+        ) : (
+          <DropdownMenuItem onClick={() => signIn('google')}>
+            <LogInIcon className='mr-2' /> Sign In
+          </DropdownMenuItem>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
 
 export function Header() {
-  const session = useSession()
-
   return (
-    <header>
-      <div>
-        {session.data ? (
-          <Button onClick={() => signOut()}>Sign Out</Button>
-        ) : (
-          <Button onClick={() => signIn('google')}>Sign In</Button>
-        )}
-        {session.data?.user?.name}
-        <ModeToggle />
+    <header className='py-2 bg-slate-100 dark:bg-slate-900 container mx-auto'>
+      <div className='flex justify-between items-center'>
+        <Link
+          className='flex gap-2 items-center text-xl hover:underline'
+          href='/'
+        >
+          <Image
+            src='/icon.png'
+            width='60'
+            height='60'
+            alt='the application icon of a magnifying glass'
+          />
+          DevFinder
+        </Link>
+        <div className='flex items-center gap-4'>
+          <AccountDropdown />
+          <ModeToggle />
+        </div>
       </div>
     </header>
   )
